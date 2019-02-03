ba5fbdacf117a4bea5bf9841f1c24f29d3d7bdb9
Ricardo A. Gorosito <rgorosito@gmail.com>
2019-02-03 17:38:16 -0300

    se llama dronin y no prueba

--- docker/build.sh
+++ docker/build.sh
@@ -7,6 +7,6 @@ thispath=$(dirname $0)
 cd $thispath
 
 realparentpath=$(realpath ../)
-podman build -t prueba -v ${realparentpath}:/dRonin.git .
+podman build -t dronin -v ${realparentpath}:/dRonin.git .
 
 cd -
